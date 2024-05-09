c269d4cb55faa132eca37fc667fc19d3e5aef800
[Sergi López] <[socelsergi@gmail.com]>
2024-05-09 13:23:37 +0200

    Hasta inverso

--- Ex2.php
+++ Ex2.php
@@ -1,4 +1,22 @@
-<html>
+<?php
+//hi
+$saludos = "Hello, World\n";
+echo $saludos.PHP_EOL . "<br>";
 
-<h1>Alo</h1>
-</html>
+//mayus
+echo $saludos = strtoupper($saludos).PHP_EOL  . "<br>";
+
+//length
+echo strlen($saludos).PHP_EOL . "<br>";
+
+//invers
+/*for($i = 0; $i < strlen($saludos); $i++){}*/
+strrev($saludos).PHP_EOL . "<br>";
+
+
+
+
+
+
+
+?>
